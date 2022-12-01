8f1a392edc9a857808a76d2d45b51439b0b3d9e8
Spencer Fairchild <fairchildspencer@gmail.com>
2022-11-30 22:37:59 -0700

    Clean everything up

--- day_one/day_one.rb
+++ day_one/day_one.rb
@@ -3,18 +3,9 @@ file_data = File.read('input_one.txt')
 elves = file_data.split("\n\n")
 
 # part one
-calorie_count = elves.map do | elf | 
-	items = elf.split.map(&:to_i)
-	elf = items.sum
-end
+calorie_count = elves.map { | elf | elf = elf.split.map(&:to_i).sum }
 
 puts (calorie_count.max)
 
 # part two
-top_elves = []
-for i in 0..2
-	top_elves.push(calorie_count.max)
-	calorie_count.delete(calorie_count.max)
-end
-
-puts(top_elves.sum)
+puts calorie_count.sort_by {|count| -count}.take(3).sum
